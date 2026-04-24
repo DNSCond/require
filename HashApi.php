8e2739fa81c1d90a5bf9a003876c162f03aa9105
DNSCond <condi7002@gmail.com>
2026-04-24 17:03:26 +0200

    sha384-

--- HashApi.php
+++ HashApi.php
@@ -31,11 +31,11 @@ function sendHashApi(string $string, bool $override = false): array
     header('vary: User-Agent', false);
     if ($override || ($_SERVER['HTTP_USER_AGENT'] === 'FaviEdge/25.5.19')) {
         header("FX-HashApi-Nonce: " . nonceBase64());
-        $sha256 = sha256Base64($string);
-        $sha384 = sha384Base64($string);
-        $sha512 = sha512Base64($string);
-        $sha1 = base64_encode(sha1($string, true));
-        header("FX-HashApi-CHash: sha256-$sha256, sha384-$sha384, sha512-$sha512, sha1-$sha1");
+        $sha256 = 'sha256-' . sha256Base64($string);
+        $sha384 = 'sha384-' . sha384Base64($string);
+        $sha512 = 'sha512-' . sha512Base64($string);
+        $sha1 = 'sha1-' . base64_encode(sha1($string, true));
+        header("FX-HashApi-CHash: $sha256, $sha384, $sha512, $sha1");
         header("FX-HashApi-DateTime: " . gmdate("D, d M Y H:i:s \\G\\M\\T", $_SERVER['REQUEST_TIME']));
         return array("sha256" => $sha256, "sha384" => $sha384, "sha512" => $sha512, "sha1" => $sha1);
     } else return array();
